58dcae4c8045d87feaad1ac75852f521d3862fb2
JDStraughan <jdstraughan@gmail.com>
2012-12-11 13:20:38 -0600

    Basic rails new install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GroundFloor::Application.config.secret_token = '432bce0d44dec68589e1a9f3e44d89533f5d98f47ff7b0bcf0b756e9d99425df017f9726089ddbcab59c2d043de923c623cd4cac05b3787711eea38b32c7ac20'
