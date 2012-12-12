774bd85e1c77a54b299c63a70f994eb57a99b341
JDStraughan <jdstraughan@gmail.com>
2012-12-11 22:27:04 -0600

    Make devise happy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ GroundFloor::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # Make devise happy
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
