4157964606651fa620b221ff8551192e221fa093
JDStraughan <jdstraughan@gmail.com>
2012-12-12 13:24:21 -0600

    Added basic user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,15 @@
 
 FactoryGirl.define do
   factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    password "foobar"
+    email { "#{first_name}.#{last_name}@example.com" }
+    is_admin false
+    
+    factory :admin do
+      is_admin true
+    end
   end
+  
 end
