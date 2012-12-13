b5c84d3529518b9853f3d0385bca7a6fbe7434af
JDStraughan <jdstraughan@gmail.com>
2012-12-13 14:25:58 -0600

    Updated spec helper to clean db, use factory girl, updated options, and require capybara/rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -23,7 +24,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
@@ -36,5 +37,26 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
   
+  # Factory girl said so
+  config.include FactoryGirl::Syntax::Methods
+  
   config.include Capybara::DSL
+  
+  # Include devise test helpers
+  # config.include Devise::TestHelpers, :type => :controller
+  # config.extend ControllerMacros, :type => :controller
+
+  # Clean up db since we are not using transactional_fixtures
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:all) do
+    DatabaseCleaner.start
+  end
+  
+  config.after(:all) do
+    DatabaseCleaner.clean
+  end
+
 end
