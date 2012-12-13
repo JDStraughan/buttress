3d824b4f57ba2f61df1c0089f9916f5802df40f2
JDStraughan <jdstraughan@gmail.com>
2012-12-13 09:59:49 -0600

    Add flash class application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def flash_class(level)
+    case level
+    when :notice then "info"
+    when :error then "error"
+    when :alert then "warning"
+    when :success then "success"
+    end
+  end
+  
 end
