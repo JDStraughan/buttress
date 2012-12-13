dacc7509ac1238436b16f576b22e76d299e7e0ed
Jason Ellis <jason.ellis@yahoo.com>
2012-12-13 10:04:24 -0600

    Refactoring the routes file to use better devise locations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Buttress::Application.routes.draw do
-  devise_for :users
 
+  # Routes for devise users (login logout register..etc)
+  devise_for :users, :path => '', :path_names => {
+      :sign_in => 'login',
+      :sign_out => 'logout',          # must be method :DELETE
+      :registration => 'register',    # /register POST
+      :sign_up => 'new'               # /register/new
+  }
+
+  # Root path
   root :to => "home#index"
+
 end
